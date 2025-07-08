71b5456cc5bed2ab983ce8c376fd066d2c962a06
Muhammed Ajmal <ajmalkunjammed@gmail.com>
2025-07-09 00:10:44 +0530

    minor change middware

--- services/auth/src/middleware/authenticate.ts
+++ services/auth/src/middleware/authenticate.ts
@@ -1,25 +1,32 @@
 import { RequestHandler } from "express";
 import appAssert from "../utils/appAssert";
-import { UNAUTHORIZED } from "../constants/http";
 import AppErrorCode from "../constants/appErrorCode";
+import { UNAUTHORIZED } from "../constants/http";
 import { verifyToken } from "../utils/jwt";
-import { AccessTokenPayload } from "../utils/jwt";
-import { Types } from "mongoose"; 
-const authenticate:RequestHandler=(req,res,next)=>{
-    const accessToken=req.cookies.accessToken as string|undefined;
-    appAssert(accessToken,UNAUTHORIZED,'Not Autherized',AppErrorCode.InvalidAccesToken);
+import mongoose from "mongoose";
+import { unknown } from "zod";
 
-    const {error,payload}=verifyToken<AccessTokenPayload>(accessToken);
-    appAssert(
-        payload && payload.userId && payload.sessionId,
-        UNAUTHORIZED,
-        error==='jwt expired'?'token expired':'Invalid Access token',
-        AppErrorCode.InvalidAccesToken
-    );
-   req.userId=payload.userId;
-   req.sessionId=payload.sessionId;
-   next()
+// wrap with catchErrors() if you need this to be async
+const authenticate: RequestHandler = (req, res, next) => {
+  const accessToken = req.cookies.accessToken as string | undefined;
+  appAssert(
+    accessToken,
+    UNAUTHORIZED,
+    "Not authorized",
+    AppErrorCode.InvalidAccesToken
+  );
 
-}
+  const { error, payload } = verifyToken(accessToken);
+  appAssert(
+    payload,
+    UNAUTHORIZED,
+    error === "jwt expired" ? "Token expired" : "Invalid token",
+    AppErrorCode.InvalidAccesToken
+  );
+
+  req.userId = payload.userId as unknown as mongoose.Types.ObjectId;
+  req.sessionId = payload.sessionId as unknown as mongoose.Types.ObjectId;
+  next();
+};
 
 export default authenticate;
